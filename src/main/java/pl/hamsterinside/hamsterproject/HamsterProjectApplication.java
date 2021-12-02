257ec12efaa2543c8f4289b3708eaf149fa64f0b
Arek <abudkowski@interia.pl>
2021-12-02 07:02:09 +0100

    first

---
+++ src/main/java/pl/hamsterinside/hamsterproject/HamsterProjectApplication.java
@@ -0,0 +1,13 @@
+package pl.hamsterinside.hamsterproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HamsterProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HamsterProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/hamsterinside/hamsterproject/HamsterProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.hamsterinside.hamsterproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HamsterProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
