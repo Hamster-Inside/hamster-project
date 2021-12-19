7b91a6d5bd1c3613c80664e2a9dfe68d67a350bb
Hamster-Inside <arkadiusz.budkowski.dev@gmail.com>
2021-12-19 20:49:09 +0100

    working post button, getting msg back

--- src/main/java/pl/hamsterinside/hamsterproject/controllers/WebController.java
+++ src/main/java/pl/hamsterinside/hamsterproject/controllers/WebController.java
@@ -1,19 +1,35 @@
 package pl.hamsterinside.hamsterproject.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
-    @Controller
-    public class WebController {
-        @RequestMapping(value = "/index")
-        public String index() {
-            return "index";
-        }
+@Controller
+public class WebController {
 
-        @RequestMapping(value = "/hamtaro")
-        public String hamtaro(){
-        String message = "wow";
-            return "hamtaro";
-        }
+    Logger logger = LoggerFactory.getLogger(WebController.class);
+    @RequestMapping(value = "/index")
+    public String index() {
+        return "index";
     }
+
+    @RequestMapping(value = "/hamtaro")
+    public String hamtaro() {
+        String message = "wow";
+        return "hamtaro";
+    }
+
+    @RequestMapping(value = "/hamtaro", method = RequestMethod.POST, params = "FormSubmit1")
+    public String submit(Model model) {
+        model.addAttribute("message", "KOKOS");
+        logger.info("Submit done");
+        return "hamtaro";
+    }
+
+}
 
